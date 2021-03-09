8d0fa2ec67add45bef27d95c995d7565de7d5600
MajahaM <majahamkhulisi@gmail.com>
2021-03-09 05:38:55 +0200

    first commit; with app clean-up, addition of components and pages

---
+++ src/components/collection-preview/collection-preview.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.style.scss';
+
+export default function CollectionPreview({title, items}){
+    return(
+        <div className='collection-preview'>
+            <h1 className='title'>{title}</h1>
+            <di className='preview'>
+            {
+                items.filter((_, idx) => idx < 4).map(({id,...otherItems}) => 
+                (
+                    <CollectionItem key={id} {...otherItems}/>
+                ))
+            }
+            </di>           
+        </div>
+    );
+}
---
+++ src/pages/shop/shop.component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import SHOP_DATA from './shop.data';
+
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            collections: SHOP_DATA
+        };       
+    }
+    render(){
+        const {collections} = this.state;
+        return <div>
+            {
+                collections.map(({id, ...otherProps})=>{
+                    return (<CollectionPreview key={id} {...otherProps}/>)
+                })
+            }
+        </div>
+    }
+}
+export default ShopPage;
