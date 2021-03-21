9ec2d4cc423e4dc041efadb723cb3510390f4819
MajahaM <majahamkhulisi@gmail.com>
2021-03-21 21:04:23 +0200

    Added static.json on root directory with custom routes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });    
 }
 
